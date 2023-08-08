02b30380f58cc8d369c9b4d6f2358db342204e2c
bioinfoark <136152523+bioinfoark@users.noreply.github.com>
2023-08-08 04:29:48 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -9,21 +9,21 @@ const presets = [
 
 function App() {
   const [switches, setSwitches] = useState([
-    { label: "有钱", isOn: false, color: "#06D6A0" },
-    { label: "漂亮", isOn: false, color: "#ffadad" },
-    { label: "性格好", isOn: false, color: "#ffd6a5" },
+    { label: "Sleep", isOn: false, color: "#06D6A0" },
+    { label: "Study", isOn: false, color: "#ffadad" },
+    { label: "Social", isOn: false, color: "#ffd6a5" },
   ]);
 
   const handleToggle = (currentIndex) => () => {
     setSwitches((prevSwitches) => {
       const switchesOn = prevSwitches.filter((item) => item.isOn);
-      if (switchesOn.length < 2) {
-        return prevSwitches.map((item, index) =>
-          index === currentIndex ? { ...item, isOn: !item.isOn } : item
-        );
-      } else if (prevSwitches[currentIndex].isOn) {
+      if (prevSwitches[currentIndex].isOn) {
         return prevSwitches.map((item, index) =>
           index === currentIndex ? { ...item, isOn: false } : item
+        );
+      } else if (switchesOn.length < 2) {
+        return prevSwitches.map((item, index) =>
+          index === currentIndex ? { ...item, isOn: true } : item
         );
       } else {
         const switchOffIndex =
@@ -66,19 +66,21 @@ function App() {
           </div>
         ))}
       </div>
-      <div className="inputs">
-        {switches.map((item, index) => (
-          <input
-            value={item.label}
-            onChange={handleChange(index)}
-            placeholder="输入你的选项"
-          />
-        ))}
-      </div>
-      <div className="presets">
-        {presets.map((preset) => (
-          <button onClick={applyPreset(preset)}>{preset.title}</button>
-        ))}
+      <div className="right-panel">
+        <div className="inputs">
+          {switches.map((item, index) => (
+            <input
+              value={item.label}
+              onChange={handleChange(index)}
+              placeholder="输入你的选项"
+            />
+          ))}
+        </div>
+        <div className="presets">
+          {presets.map((preset) => (
+            <button onClick={applyPreset(preset)}>{preset.title}</button>
+          ))}
+        </div>
       </div>
     </div>
   );
